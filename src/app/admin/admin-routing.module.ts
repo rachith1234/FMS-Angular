239116f6310c499587bc65efcea24040cb53adf6
rachith1234 <35251263+rachith1234@users.noreply.github.com>
2023-03-31 16:01:37 +0530

    updates

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CustomerOrdersComponent } from './customer-orders/customer-orders.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PurchaseStockComponent } from './purchase-stock/purchase-stock.component';
+import { DashboardPannelComponent } from './dashboard-pannel/dashboard-pannel.component';
 
 const routes: Routes = [
   {path:'admin' , component:DashboardComponent},
 
   {path:'admin',
     children:[
-      {path:'customer-orders', component:CustomerOrdersComponent},
+      {path:'app-dashboard-pannel', component:DashboardPannelComponent},
       {path:'purchase-stock', component:PurchaseStockComponent}
     ]
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { OurProductsComponent } from './our-products/our-products.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path:'main', component : MainComponent},
+  {path:'', component : MainComponent},
   {path:'register', component : RegisterComponent},  
   {path:'our-products', component : OurProductsComponent},
-  {path:'footer', component : FooterComponent}
+  {path:'footer', component : FooterComponent},
+  { path: 'admin', component: DashboardComponent }
   
 
   
