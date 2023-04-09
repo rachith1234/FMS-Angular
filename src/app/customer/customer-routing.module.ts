4977f6646ae8fa7be65532457e7d732ff11df88c
rachith1234 <35251263+rachith1234@users.noreply.github.com>
2023-04-09 21:02:01 +0530

    set orders component routes

--- src/app/customer/customer-routing.module.ts
+++ src/app/customer/customer-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerComponent } from './customer.component';
+import { OrdersComponent } from './orders/orders.component';
 
 const routes: Routes = [
   {path:'' ,
    component:CustomerComponent,
+   children:[
+    {path:'orders' ,component: OrdersComponent },
+    
+   ],
+
   
 }
 ];
--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
+import { OrdersComponent } from './orders/orders.component';
 
 
 @NgModule({
   declarations: [
-    CustomerComponent
+    CustomerComponent,
+    OrdersComponent
   ],
   imports: [
     CommonModule,
