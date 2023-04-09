e86b0e5005ea2c6c0f1fac5644f591a24ef90cb2
rachith1234 <35251263+rachith1234@users.noreply.github.com>
2023-04-09 22:28:57 +0530

    set farmer routes

--- src/app/farmer/farmer-routing.module.ts
+++ src/app/farmer/farmer-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FarmerComponent } from './farmer.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { CurrentStockComponent } from './current-stock/current-stock.component';
+import { SoldItemsComponent } from './sold-items/sold-items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component:FarmerComponent,
+   children:[
+    {path:'' , component:AddProductComponent,},
+    {path:'add-product' ,component: AddProductComponent},
+    {path:'current-stock' ,component: CurrentStockComponent},
+    {path:'sold-items' ,component: SoldItemsComponent},
+    
+    
+   ],
+
+  
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
