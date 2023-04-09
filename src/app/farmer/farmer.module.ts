b8134c6226c1d401b40e2e75cd04e31b4af861fd
rachith1234 <35251263+rachith1234@users.noreply.github.com>
2023-04-09 22:11:32 +0530

    create farmer/sold-items component

--- src/app/farmer/farmer.module.ts
+++ src/app/farmer/farmer.module.ts
@@ -5,13 +5,15 @@ import { FarmerRoutingModule } from './farmer-routing.module';
 import { FarmerComponent } from './farmer.component';
 import { CurrentStockComponent } from './current-stock/current-stock.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { SoldItemsComponent } from './sold-items/sold-items.component';
 
 
 @NgModule({
   declarations: [
     FarmerComponent,
     CurrentStockComponent,
-    AddProductComponent
+    AddProductComponent,
+    SoldItemsComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/farmer/sold-items/sold-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sold-items',
+  templateUrl: './sold-items.component.html',
+  styleUrls: ['./sold-items.component.css']
+})
+export class SoldItemsComponent {
+
+}
